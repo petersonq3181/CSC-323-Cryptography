75c67a1287f3196277a92419a9c6f5945c82d6d3
Quinn Peterson <qap2001@gmail.com>
2024-01-13 20:50:54 -0800

    lab0: task ii a

--- Lab0/lab0.py
+++ Lab0/lab0.py
@@ -15,14 +15,26 @@ def bytes2base64(s):
     return base64.b64encode(s).decode('utf-8')
 
 
-byte_string = b"Hello, world!"
-hex_string = bytes2hex(byte_string)
-print("Hex Encoded:", hex_string)
-decoded_bytes = hex2bytes(hex_string)
-print("Decoded Bytes:", decoded_bytes)
+# Task II. A. Implement XOR
+def xor_strings(s, key):
+    repeated_key = (key * (len(s) // len(key) + 1))[:len(s)]
+    return bytes([input_byte ^ key_byte for input_byte, key_byte in zip(s, repeated_key)])
 
-base64_str = bytes2base64(byte_string)
-print("Base64 Encoded:", base64_str)
-decoded_bytes = base642bytes(base64_str)
-print("Decoded Bytes:", decoded_bytes)
+
+# Task II. B. Single-byte XOR
+
+
+
+
+if __name__ == "__main__": 
+    byte_string = b"Hello, world!"
+    hex_string = bytes2hex(byte_string)
+    print("Hex Encoded:", hex_string)
+    decoded_bytes = hex2bytes(hex_string)
+    print("Decoded Bytes:", decoded_bytes)
+
+    base64_str = bytes2base64(byte_string)
+    print("Base64 Encoded:", base64_str)
+    decoded_bytes = base642bytes(base64_str)
+    print("Decoded Bytes:", decoded_bytes)
 
