b7c55626b9b43a1b704439f716dda6d109f7c805
Quinn Peterson <qap2001@gmail.com>
2024-02-17 15:38:50 -0800

    lab2: task ii Identify ECB Mode

--- Lab2/taskii.py
+++ Lab2/taskii.py
@@ -37,8 +37,41 @@ def read_and_decode(fn):
         decoded = base64.b64decode(encoded)
         return decoded
 
-# testing w/ TaskII.A.txt 
+# --- Implement ECB Mode
 key = b"CALIFORNIA LOVE!"
 encoded = read_and_decode('Lab2.TaskII.A.txt')
 decrypted = ecb_decrypt(key, encoded)
 print(decrypted.decode('utf-8'))
+
+
+
+def detect_ecb(ciphertext, block):
+    blocks = [ciphertext[i:i + block] for i in range(0, len(ciphertext), block)]
+    num_unique = len(set(blocks))
+    return len(blocks) != num_unique
+
+def find_ecb(fn):
+    with open(fn, 'r') as file:
+        lines = file.readlines()
+
+    # ignoring the first 54 bytes which are the BMP header
+    for i, line in enumerate(lines):
+        ciphertext = bytes.fromhex(line.strip())[54:]
+
+        # with open(f"ecb_encrypted_{i}.bmp", 'wb') as image_file:
+        #     image_file.write(bytes.fromhex(line.strip()))
+
+        if detect_ecb(ciphertext, AES.block_size):
+            return i, line.strip()
+    return None, None
+
+# --- Identify ECB Mode
+ecb_idx, ecb_line = find_ecb('Lab2.TaskII.B.txt')
+
+if ecb_line is not None:
+    print(f"ECB encrypted line is at index {ecb_idx}")
+    with open('ecb_encrypted_image.bmp', 'wb') as image_file:
+        image_file.write(bytes.fromhex(ecb_line))
+else:
+    print("No ECB encrypted image found")
+
