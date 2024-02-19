caca146a518354824c585a5c979481a5a5cbd390
Quinn Peterson <qap2001@gmail.com>
2024-02-19 12:38:59 -0800

    lab2: task ii seemingly working get cookie version

--- Lab2/ECB-Cookies/s.py
+++ Lab2/ECB-Cookies/s.py
@@ -1,47 +1,46 @@
 import requests
-import server
-
-# Create a session object
-session = requests.Session()
 
 url = 'http://0.0.0.0:8080/'
 
-# Register a user
-reg_data = {
-    'user': 'a',
-    'password': '12'
-}
-session.get(url + 'register')
-reg_response = session.post(url + 'register', data=reg_data)
+# Create a session object
+with requests.Session() as session:
+    
+    # Register user
+    reg_data = {
+        'user': 'a', 
+        'password': '12' 
+    }
+    session.get(url + 'register')  # Get the register page to set initial cookies
+    reg_response = session.post(url + 'register', data=reg_data)
+    
+    if reg_response.ok:
+        print("Registration successful")
+        print(reg_response.text)
+    else:
+        print("Registration failed")
 
-if reg_response.ok:
-    print("Registration successful")
-    print(reg_response.text)
-else:
-    print("Registration failed")
+    # Login as the registered user
+    login_data = {
+        'user': 'a', 
+        'password': '12' 
+    }
+    session.get(url)  # Get the main page to set initial cookies
+    login_response = session.post(url, data=login_data)
+    
+    if login_response.ok:
+        print("Login successful")
+        print(login_response.text)
+    else:
+        print("Login failed")
 
-login_data = {
-    'user': 'a',
-    'password': '12'
-}
-session.get(url)
-login_response = session.post(url, data=login_data)
+    # Access home page
+    home_response = session.get(url + 'home')
+    if home_response.ok:
+        print("Accessing home page")
+        print(home_response.text)
+    else:
+        print("Failed to access home page")
 
-if login_response.ok:
-    print("Login successful")
-    print(login_response.text)
-else:
-    print("Login failed")
-
-# Access the home page
-home_response = session.get(url + 'home')
-if home_response.ok:
-    print('Access to home page successful')
-    print(home_response.text)
-else:
-    print('Access to home page failed')
-
-
-
-cookie = session.get(url + 'get_cookie')
-print(cookie.text)
+    # Get cookie
+    cookie_response = session.get(url + 'get_cookie')
+    print("Cookie:", cookie_response.text)
