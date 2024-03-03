2d8c09129b3f778d0962ae0fe5544725d0665522
Quinn Peterson <qap2001@gmail.com>
2024-03-03 13:08:37 -0800

    lab3: taskiii cur funky state

--- Lab3/EC-SmallSubgroup/test.py
+++ Lab3/EC-SmallSubgroup/test.py
@@ -24,57 +24,62 @@ unique_points = set(acc)
 for i, p in enumerate(unique_points):
     print(i, p)
 
-# # ----- talk to admin 
-# admin_public = (121827448578773748305120722784972190963, 112548776019462441772570757329996227364)
-# admin_public_point = crypto.EccAlgPoint(curve=crypto.curve, x=admin_public[0], y=admin_public[1])
+# ----- talk to admin 
+admin_public = (121827448578773748305120722784972190963, 112548776019462441772570757329996227364)
+admin_public_point = crypto.EccAlgPoint(curve=crypto.curve, x=admin_public[0], y=admin_public[1])
 
-# usr = 'Admin'
-# msg = 'hello admin!!' 
+usr = 'Admin'
+msg = 'hello admin!!' 
 
-# my_private, my_public = crypto.gen_keys()
-# # my_public = unique_points[0] # TODO 
-# print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
+my_private, my_public = crypto.gen_keys()
+# my_public = unique_points[0] # TODO 
+print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
 
-# shared_key = crypto.get_shared_key(admin_public_point, my_private)
-# print(f'shared key:\n\t {shared_key}\n')
+shared_key = crypto.get_shared_key(admin_public_point, my_private)
+print(f'shared key:\n\t {shared_key}\n')
 
-# hmac = crypto.calculate_hmac(msg, shared_key)
-# print(f'hmac:\n\t {hmac.hexdigest()}')
+hmac = crypto.calculate_hmac(msg, shared_key)
+print(f'hmac:\n\t {hmac.hexdigest()}')
 
-# with requests.Session() as session:
+with requests.Session() as session:
 
-#     def submit_msg(hmac, x, y):
-#         data = {
-#             'recipient': usr,
-#             'message': msg,
-#             'hmac': hmac.hexdigest(),
-#             'pkey_x': x,
-#             'pkey_y': y
-#         }
-#         res = session.post(url + 'submit', data=data)
+    def submit_msg(hmac, x, y):
+        data = {
+            'recipient': usr,
+            'message': msg,
+            'hmac': hmac.hexdigest(),
+            'pkey_x': x,
+            'pkey_y': y
+        }
+        res = session.post(url + 'submit', data=data)
 
-#         soup = BeautifulSoup(res.text, 'html.parser')
-#         hmac_text = soup.find('font', string=lambda t: "HMAC" in t).text if soup.find('font', string=lambda t: "HMAC" in t) else None
-#         question_text = soup.find('font', string=lambda t: "What do you want?" in t).text if soup.find('font', string=lambda t: "What do you want?" in t) else None
-#         return hmac_text, question_text
+        soup = BeautifulSoup(res.text, 'html.parser')
+        hmac_text = soup.find('font', string=lambda t: "HMAC" in t).text if soup.find('font', string=lambda t: "HMAC" in t) else None
+        question_text = soup.find('font', string=lambda t: "What do you want?" in t).text if soup.find('font', string=lambda t: "What do you want?" in t) else None
+        return hmac_text, question_text
 
-#     hmac_texts = [submit_msg(hmac, str(my_public.x), str(my_public.y))]
-#     hmacs = []
+    hmac_texts = [submit_msg(hmac, str(my_public.x), str(my_public.y))]
+    hmacs = []
 
-#     for my_public in unique_points: 
-#         # print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
+    for my_public in unique_points: 
+        # print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
 
-#         shared_key = crypto.get_shared_key(admin_public_point, my_private)
-#         # print(f'shared key:\n\t {shared_key}\n')
+        shared_key = crypto.get_shared_key(admin_public_point, my_private)
+        # print(f'shared key:\n\t {shared_key}\n')
 
-#         hmac = crypto.calculate_hmac(msg, shared_key)
-#         # print(f'hmac:\n\t {hmac.hexdigest()}')
+        hmac = crypto.calculate_hmac(msg, shared_key)
+        # print(f'hmac:\n\t {hmac.hexdigest()}')
 
-#         res = submit_msg(hmac, str(my_public.x), str(my_public.y))
-#         hmac_texts.append(res)
-#         hmacs.append(res[0].split(":")[1].strip())
+        try:
+            res = submit_msg(hmac, str(my_public.x), str(my_public.y))
+        except:
+            res = submit_msg(hmac, None, None)
 
-#     for ele in hmac_texts:
-#         print(ele)
-#     for ele in hmacs: 
-#         print(ele)
+        print(res)
+        # hmac_texts.append(res)
+        # hmacs.append(res[0].split(":")[1].strip())
+
+    for ele in hmac_texts:
+        print(ele)
+    for ele in hmacs: 
+        print(ele)
