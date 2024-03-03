518cf4cafae2240ccc9ab324f358f8331a6061c5
Quinn Peterson <qap2001@gmail.com>
2024-03-02 20:40:19 -0800

    lab3: task iii test refactor

--- Lab3/EC-SmallSubgroup/test.py
+++ Lab3/EC-SmallSubgroup/test.py
@@ -1,40 +1,18 @@
-import crypto 
-import matplotlib.pyplot as plt
-import numpy as np
-
+import crypto
 
 curve = crypto.Curve(a=-95051, b=11279326, field=233970423115425145524320034830162017933)
 base_point = crypto.EccAlgPoint(curve=curve, x=182, y=85518893674295321206118380980485522083)
 bp_order = 29246302889428143187362802287225875743
 
-
 point = crypto.EccAlgPoint(curve=crypto.curve, x=16349894185180983439102154383611486412, y=224942997200586455214256137069604954919)
 
-accx = []
-accy = []
-for i in range(1, 100):
+acc = []
+for i in range(1, 1000):
     try:
-        accx.append(crypto.EccPoint.__mul__(point, i).x) 
-        accy.append(crypto.EccPoint.__mul__(point, i).y) 
+        acc.append((crypto.EccPoint.__mul__(point, i).x, crypto.EccPoint.__mul__(point, i).y))
     except: 
         pass 
 
-print(set(accx))
-print(set(accy))
-
-# Convert data to numpy array for easier handling
-data_np = np.array(accx, dtype=np.float64)
-
-# Plotting
-plt.figure(figsize=(10, 6))
-
-# Since the data spans several orders of magnitude, a logarithmic scale might be more appropriate
-# We'll add a small constant to handle any data points that might be 0 or negative
-plt.hist(data_np, bins=20, log=True, color='skyblue', edgecolor='black')
-
-plt.xlabel('Value')
-plt.ylabel('Frequency')
-plt.title('Distribution of Data')
-
-# Show plot
-plt.show()
+uniq_points = set(acc)
+for i, p in enumerate(uniq_points):
+    print(i, p)
