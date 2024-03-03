4fad3fe8bf62c806372e78fcbbb085b10f1e1885
Quinn Peterson <qap2001@gmail.com>
2024-03-02 21:55:16 -0800

    lab3: task iii alway producing same hmacs!

--- Lab3/EC-SmallSubgroup/test.py
+++ Lab3/EC-SmallSubgroup/test.py
@@ -1,5 +1,6 @@
 import crypto
 import requests
+from bs4 import BeautifulSoup
 
 url = 'http://0.0.0.0:8080/'
 
@@ -15,17 +16,18 @@ for i in range(0, 1000):
     try:
         acc.append((crypto.EccPoint.__mul__(point, i).x, crypto.EccPoint.__mul__(point, i).y))
     except: 
-        print(crypto.EccPoint.__mul__(point, i).__str__())
+        # print(crypto.EccPoint.__mul__(point, i).__str__())
+        pass 
 
 # note: prints 7 elements instead of 8 b/c missing the origin 
 ps = set(acc)
 unique_points = []
 for i, p in enumerate(ps):
-    print(i, p)
+    # print(i, p)
     unique_points.append(crypto.EccAlgPoint(curve=crypto.curve, x=p[0], y=p[1]))
 
 # ----- talk to admin 
-admin_public = (5857514870807797964012693241104587497, 193396171777314066986828010215107005104)
+admin_public = (228586928728760623489332690628331395579, 80025156566348652611156132435508764067)
 admin_public_point = crypto.EccAlgPoint(curve=crypto.curve, x=admin_public[0], y=admin_public[1])
 
 usr = 'Admin'
@@ -33,28 +35,44 @@ msg = 'hello admin!!'
 
 my_private, my_public = crypto.gen_keys()
 # my_public = unique_points[0] # TODO 
-print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
+# print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
 
-shared_key = crypto.get_shared_key(admin_public_point, my_private)
-print(f'shared key:\n\t {shared_key}\n')
+# shared_key = crypto.get_shared_key(admin_public_point, my_private)
+# print(f'shared key:\n\t {shared_key}\n')
 
-hmac = crypto.calculate_hmac(msg, shared_key)
-print(f'hmac:\n\t {hmac.hexdigest()}')
-
-# reply, gen_hmac = server.user_msg(usr, msg, 
-#     given_hmac=hmac.hexdigest(), 
-#     pub_key_x=str(my_public.x), 
-#     pub_key_y=str(my_public.y))
-# print(f'reply:\n\t{reply} \ngen_hmac:\n\t {gen_hmac}')
+# hmac = crypto.calculate_hmac(msg, shared_key)
+# print(f'hmac:\n\t {hmac.hexdigest()}')
 
 with requests.Session() as session:
-    data = {
-        'recipient': usr,
-        'message': msg,
-        'hmac': hmac.hexdigest(),
-        'pkey_x': str(my_public.x),
-        'pkey_y': str(my_public.y)
-    }
-    res = session.post(url + 'submit', data=data)
-    print(res)
-    print(res.text)
+
+    def submit_msg(hmac, x, y):
+        data = {
+            'recipient': usr,
+            'message': msg,
+            'hmac': hmac.hexdigest(),
+            'pkey_x': str(my_public.x),
+            'pkey_y': str(my_public.y)
+        }
+        res = session.post(url + 'submit', data=data)
+
+        soup = BeautifulSoup(res.text, 'html.parser')
+        hmac_text = soup.find('font', string=lambda t: "HMAC" in t).text if soup.find('font', string=lambda t: "HMAC" in t) else None
+        question_text = soup.find('font', string=lambda t: "What do you want?" in t).text if soup.find('font', string=lambda t: "What do you want?" in t) else None
+        print("HMAC text:", hmac_text)
+        print("Question text:", question_text)
+        return hmac_text
+
+    hmac_texts = []
+    for my_public in unique_points: 
+        print(f'my private and public:\n\t {my_private}\n\t {my_public}\n')
+
+        shared_key = crypto.get_shared_key(admin_public_point, my_private)
+        print(f'shared key:\n\t {shared_key}\n')
+
+        hmac = crypto.calculate_hmac(msg, shared_key)
+        print(f'hmac:\n\t {hmac.hexdigest()}')
+
+        hmac_texts.append(submit_msg(hmac, str(my_public.x), str(my_public.y)))
+
+    for ele in hmac_texts:
+        print(ele)
