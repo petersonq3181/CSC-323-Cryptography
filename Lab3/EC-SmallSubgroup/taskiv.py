a7dbde489e11e2e034c6540807b244907cd7471d
Quinn Peterson <qap2001@gmail.com>
2024-03-04 16:58:16 -0800

    lab3: task iv step 1 cur

--- Lab3/EC-SmallSubgroup/taskiv.py
+++ Lab3/EC-SmallSubgroup/taskiv.py
@@ -2,6 +2,39 @@ from sympy.ntheory import factorint
 from taskii import *
 import crypto
 
+def factors_2_points(factors):
+    total_product = 1
+    for key, value in factors.items():
+        total_product *= key**value
+
+    fs_otherprods = {}
+    for key in factors:
+        if factors[key] > 1:
+            new_value = total_product // (key ** factors[key])
+        else:
+            new_value = total_product // key
+        fs_otherprods[key] = new_value
+
+    points = {}
+    for k, v in fs_otherprods.items():
+        X, Y = find_random_point_on_curve(potential_curves[1][0], potential_curves[1][1], potential_curves[1][2])
+        new_point_orig = crypto.EccAlgPoint(curve=crypto.curve, x=X, y=Y)
+        new_point = new_point_orig * v
+        points[k] = new_point
+
+    return points
+
+def test_point_order(point, expected_order):
+    acc = []
+    for i in range(0, expected_order * 11):
+        mp = point * i 
+
+        acc.append((i % expected_order, mp))
+
+    unique_points = set(acc)
+    return len(unique_points) == expected_order
+
+
 # potential curves (M = B (ie. B != server curve's B))
 # (A, B, Field, Order)
 potential_curves = [
@@ -25,54 +58,29 @@ uniq_pf = {key for f in factors for key in f.keys() if key < 65536}
 print(uniq_pf)
 print(len(uniq_pf))
 
+# make sure prod of all unique factors is greater than server's curve order 
 factors_product = 1
 for ele in uniq_pf:
     factors_product *= ele 
 print('factors product', factors_product)
 print(factors_product > 29246302889428143187362802287225875743)
 
-# reconstruct the prime factors (stored per curve) that are unique 
+# turn each curve's factor dicts into {factor: point} dicts 
+f_points = []
+for f in factors:
+    f_points_dict = factors_2_points(f)
+    f_points.append(f_points_dict)
+
+# reconstruct to only save prime factors (stored per curve) that are unique 
+# across all the curves 
 curve_pfs = [[] for _ in range(len(factors))]
-for i, fs in enumerate(factors):
-    for ks in fs.keys():
-        if ks in uniq_pf:
-            curve_pfs[i].append(ks)
-            uniq_pf.remove(ks)
-
-print(curve_pfs)
-
-# # (hand-selected based on viewing print statements)
-# relevant_curves = [1, 2, 4]
+for i, fs in enumerate(f_points):
+    for factor, point in fs.items():
+        if factor in uniq_pf:
+            uniq_pf.remove(factor)
+            curve_pfs[i].append((factor, point))
 
 
-# testing :
-# take one prime factor of one curve, and also find product 
-# of all other factors 
-# pick random point on the curve 
-# multiply by number above 
-# see if point truly has order equal to the orig prime factor 
-curve2_gg = {2: 2, 7: 1, 23: 1, 37: 1, 67: 1, 607: 1, 1979: 1, 13327: 1, 13799: 1, 663413139201923717: 1}
-prime = 7 
-other_prod = 1
-for k, v in curve2_gg.items():
-    if k != prime: 
-        other_prod *= k * v 
-print(f'curve 2:\n\torder: {potential_curves[3]}\n\tprime: {prime}\n\tother_prod: {other_prod}\n\tprod: {other_prod * prime}')
+# (hand-selected based on viewing print statements)
+relevant_curves = [1, 2, 4]
 
-X, Y = find_random_point_on_curve(potential_curves[1][0], potential_curves[1][1], potential_curves[1][2])
-print(X, Y)
-
-test_point_orig = crypto.EccAlgPoint(curve=crypto.curve, x=X, y=Y)
-test_point = test_point_orig * other_prod
-print(test_point)
-
-
-acc = []
-for i in range(0, 100):
-    mp = test_point * i 
-
-    acc.append((i % prime, mp))
-
-unique_points = set(acc)
-for i, p in enumerate(unique_points):
-    print(i, p)
